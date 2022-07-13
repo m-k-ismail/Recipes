a32727aef2d5803895e012f3e35fa41426227e55
mohammadkhairelddin <40949070+mohammadkhairelddin@users.noreply.github.com>
2022-07-13 22:06:06 +0300

    Created Recipe API By Using Open API Codegen

---
+++ src/main/java/com/abn/recipe/controller/RecipeApiImpl.java
@@ -0,0 +1,39 @@
+package com.abn.recipe.controller;
+
+import com.abn.api.RecipeApi;
+import com.abn.model.Recipe;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+@RestController
+public class RecipeApiImpl implements RecipeApi {
+
+    @Override
+    public ResponseEntity<Recipe> createRecipe(Recipe recipe) {
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteRecipe(String id) {
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public ResponseEntity<Recipe> getRecipe(String id) {
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public ResponseEntity<Recipe> getRecipes(String numberOfServings, String type, Set<String> includeIngredient,
+                                             Set<String> excludeIngredient, Integer pageLimit, Integer pageOffset) {
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+    }
+
+    @Override
+    public ResponseEntity<Recipe> updateRecipe(String id, Recipe recipe) {
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+    }
+}
